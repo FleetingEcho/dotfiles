95ecf8a90d13902ce238930c6e346920b07721b8
JakeZT <jakeottawaca@gmail.com>
2024-11-19 10:15:48 -0800

    chore: Update nvim config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -22,22 +22,22 @@ return {
 	},
 
 	-- Refactoring tool
-	{
-		"ThePrimeagen/refactoring.nvim",
-		keys = {
-			{
-				"<leader>r",
-				function()
-					require("refactoring").select_refactor()
-				end,
-				mode = "v",
-				noremap = true,
-				silent = true,
-				expr = false,
-			},
-		},
-		opts = {},
-	},
+	-- {
+	-- 	"ThePrimeagen/refactoring.nvim",
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>r",
+	-- 			function()
+	-- 				require("refactoring").select_refactor()
+	-- 			end,
+	-- 			mode = "v",
+	-- 			noremap = true,
+	-- 			silent = true,
+	-- 			expr = false,
+	-- 		},
+	-- 	},
+	-- 	opts = {},
+	-- },
 
 	-- Go forward/backward with square brackets
 	{
